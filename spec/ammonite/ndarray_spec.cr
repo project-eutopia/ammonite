67d27de4fe558e426c835e5e4e954d6ca6f55df0
Christopher Locke <project.eutopia@gmail.com>
2017-08-22 14:49:05 +0900

    Update Ndarray spec for #empty

--- spec/ammonite/ndarray_spec.cr
+++ spec/ammonite/ndarray_spec.cr
@@ -2,14 +2,23 @@ require "../spec_helper"
 require "../../src/ammonite/ndarray.cr"
 
 describe Ammonite::Ndarray do
-  it "works" do
-    a = Ammonite::Ndarray(Int32).empty([4, 2])
-    a.strides.should eq [2, 1]
+  describe "#empty" do
+    it "allocates with correct attributes" do
+      a = Ammonite::Ndarray(Int32).empty([4, 2])
+      a.shape.should eq [4, 2]
+      a.size.should eq 8
+      a.elem_size.should eq 4
+      a.total_bytes.should eq 32
+      a.offset.should eq 0
+      a.strides.should eq [2, 1]
 
-    b = Ammonite::Ndarray(UInt16).empty([2, 3, 4])
-    b.strides.should eq [3*4, 4, 1]
-
-    b[1,2,3].set 14
-    b[1,2,3].value.should eq 14
+      b = Ammonite::Ndarray(UInt16).empty([2, 3, 4])
+      b.shape.should eq [2, 3, 4]
+      b.size.should eq 24
+      b.elem_size.should eq 2
+      b.total_bytes.should eq 48
+      b.offset.should eq 0
+      b.strides.should eq [3*4, 4, 1]
+    end
   end
 end
